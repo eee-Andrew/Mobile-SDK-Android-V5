d7a88fbb6272db1080d7328ebbed52f4c3a88e1b
Andrew <98215048+eee-Andrew@users.noreply.github.com>
2025-07-22 15:34:41 +0300

    Remove Python integration

--- SampleCode-V5/android-sdk-v5-sample/src/main/java/com/dji/sampleV5/aircraft/MainActivity.java
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/com/dji/sampleV5/aircraft/MainActivity.java
@@ -4,19 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!Python.isStarted()) {
-            Python.start(new AndroidPlatform(this));
-        }
-        Python py = Python.getInstance();
-        // For initial testing, simply fly to a set altitude using Python
-        py.getModule("drone_control").callAttr("fly_to_altitude", 20.0);
+        Log.d("MainActivity", "App started");
     }
 }
